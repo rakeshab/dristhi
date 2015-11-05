e6ed664d1f9e722e44f4ee6a36f2bff14f009f7f
SDevi <sridevi.g@dhanushinfotech.net>
2015-11-05 15:02:39 +0530

    Junit test caces

--- drishti-web/src/test/java/org/ei/drishti/web/controller/ANMLocationControllerTest.java
+++ drishti-web/src/test/java/org/ei/drishti/web/controller/ANMLocationControllerTest.java
@@ -40,14 +40,15 @@ public class ANMLocationControllerTest {
 
         when(httpAgent.get("http://dristhi_reporting_url/villages?anm-id=demo1")).
                 thenReturn(new HttpResponse(true,
-                        new Gson().toJson(new VillagesDTO("district", "PHC X", "phc1", "Sub Center 1", asList("village1", "village2", "village3")))));
+                        new Gson().toJson(new VillagesDTO("district", "PHCX", "phc1", "Sub Center 1",
+                                asList("village1", "village2", "village3")))));
         when(userController.currentUser()).thenReturn(user);
         when(user.getUsername()).thenReturn("demo1");
-        when(user.getRoles()).thenReturn(asList("User"));
+        when(user.getRoles()).thenReturn(asList("user"));
 
         ResponseEntity<VillagesDTO> response = controller.villagesForANM();
 
-        assertEquals(new VillagesDTO("district", "PHC X", "phc1", "Sub Center 1", asList("village1", "village2", "village3")), response.getBody());
+        assertEquals(new VillagesDTO("district", "PHCX", "phc1", "Sub Center 1", asList("village1", "village2", "village3")), response.getBody());
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 }
